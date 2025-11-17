3f6f94c34932610ca1c2d336f0e5ed59cfdde7f5
yuvraj2502 <yuvrajv2502@gmail.com>
2025-11-17 21:44:10 +0530

    basic spring boot structure
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/hackathon/mvp/infobase/InfobaseApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.mvp.infobase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfobaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfobaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/mvp/infobase/InfobaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.mvp.infobase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfobaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
